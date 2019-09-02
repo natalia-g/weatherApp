487ee30aa123bd64156fadc6f8e9607673d77409
natalia-g <nataliagrudzinska1@gmail.com>
2019-09-03 00:44:49 +0200

    first part

---
+++ script.js
@@ -0,0 +1,75 @@
+let appId = '30fe48c94183f25a5cf5b5a1d8357fc2';
+let unit = 'metric';
+
+function search(searchText){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchText}&APPID=${appId}&units=${unit}`).then(result => {
+        return result.json();
+    }).then(result => {
+        init(result);
+    })
+}
+
+function init(fromServer){
+    switch (fromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = 'url("/images/clear.jpg")';
+            break;
+
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = 'url("/images/thunderstorm.jpg")';
+            break;
+
+        case 'Clouds':
+            document.body.style.backgroundImage = 'url("/images/clouds.jpg")';
+            break;
+
+        case 'Snow':
+            document.body.style.backgroundImage = 'url("/images/snow.jpg")';
+            break;      
+
+        case 'Rain':
+        case 'Mist':
+        case 'Drizzle':  
+            document.body.style.backgroundImage = 'url("/images/rain.jpg")';      
+            break;
+
+        default:
+            break;
+    }
+
+    let description = document.getElementById('description');
+    let temperature = document.getElementById('temperature');
+    let humidity = document.getElementById('humidity');
+    let wind = document.getElementById('wind');
+    let city = document.getElementById('city-name');
+    let pressure = document.getElementById('pressure');
+
+    let resultDescription = fromServer.weather[0].description;
+    description.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+    temperature.innerHTML = Math.floor(fromServer.main.temp) + '&#176' + 'C';
+    wind.innerHTML = 'Wind: ' + Math.floor(fromServer.wind.speed) + ' m/s';
+    city.innerHTML = fromServer.name;
+    humidity.innerHTML = 'Humidity: ' + fromServer.main.humidity + '%';
+    pressure.innerHTML = 'Pressure: ' + Math.floor(fromServer.main.pressure) + ' hPa';
+    
+    setBoxPosition();
+}
+
+ function setBoxPosition() {
+    let weatherBox = document.getElementById('weather-box');
+    let weatherBoxHeight = weatherBox.clientHeight;
+    let weatherBoxWidth = weatherBox.clientWidth;
+
+    weatherBox.style.left = `calc(50% - ${weatherBoxWidth/2}px)`;
+    weatherBox.style.top = `calc(50% - ${weatherBoxHeight/2}px)`;
+    weatherBox.style.visibility = 'visible';
+}
+
+
+document.getElementById('button').addEventListener('click', () => {
+    let searchText = document.getElementById('search-text').value;
+    if(searchText){
+        search(searchText);
+    }
+})
